8e8474f2afe25af595211115599dc28b79431825
William Yang <williamyang2017@gmail.com>
2020-02-12 19:48:58 +1100

    Added fader type led controller

--- src/LEDController/Fader.jsx
+++ src/LEDController/Fader.jsx
@@ -1,33 +1,88 @@
-import WebsocketManager from "../WebsocketManager.jsx";
+import LedController from "./LedController.jsx";
 
 import React from 'react';
 import ReactDom from 'react-dom';
 
-export default class Fader extends React.Component {
+const Direction = {
+    FALLING: 0, 
+    RISING: 1
+};
+
+export default class Fader extends LedController {
     constructor(props) {
         super(props);
+        this.state = {
+            fadeValues: [],
+            fadeDirection: []
+        };
     }   
 
-    onLedChange(index) {
-        let value = this.props.leds[index];
-        let leds = [...this.props.leds];
-        let newValue = (value > 0) ? 0 : 128;
-        leds[index] = newValue;
-        this.props.setLeds(leds);
+    componentDidMount() {
+        this.interval = setInterval(() => this.updateLeds(), 33);
+    }
+
+    updateLeds() {
+        if (this.leds === undefined) {
+            return;
+        }
+        let leds = [...this.leds];
+        for (let i = 0; i < leds.length; i++) {
+            let direction = this.state.fadeDirection[i];
+            let rate = this.state.fadeValues[i];
+            switch (direction) {
+            case Direction.RISING:
+                leds[i] += rate;
+                if (leds[i] >= 128) {
+                    leds[i] = 128;
+                    this.state.fadeDirection[i] = Direction.FALLING;
+                }
+                break;
+            case Direction.FALLING:
+                leds[i] -= rate;
+                if (leds[i] <= 0) {
+                    leds[i] = 0;
+                    this.state.fadeDirection[i] = Direction.RISING;
+                }
+                break;
+            }
+        }
+        this.leds = leds;
+    }
+
+    onInfo(data) {
+        super.onInfo(data);
+        if (this.leds.length === this.state.fadeValues.length) {
+            return;
+        }
+        let fadeValues = this.leds.map(() => 0);
+        let fadeDirection = this.leds.map(() => Direction.RISING);
+        this.setState({...this.state, fadeValues: fadeValues, fadeDirection: fadeDirection});
+    }
+
+    componentWillUnmount() {
+        super.componentWillUnmount();
+        clearInterval(this.interval);
+    }
+
+    onFadeChange(index, event) {
+        let value = Number(event.target.value);
+        let fadeValues = [...this.state.fadeValues];
+        fadeValues[index] = value;
+        this.setState({...this.state, fadeValues: fadeValues});
     }
 
     createSlider(value, index) {
        return <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
-            <button className="btn btn-outline-primary btn-sm"
-                   onClick={() => this.onLedChange(index)}>
-                Toggle
-            </button>
+            <input  className="custom-range mr-2 w-90" type="range"
+                    min="0" max="20" step="1"
+                    value={value} 
+                    onChange={(ev) => this.onFadeChange(index, ev)}></input>
             <span className="badge badge-primary badge-pill">{value}</span>
        </li>;
     }
 
     render() {
-        let sliders = this.props.leds.map((d, i) => this.createSlider(d, i)); 
+        let sliders = this.state.fadeValues.map((d, i) => this.createSlider(d, i)); 
         return (
         <div>
             {sliders}
