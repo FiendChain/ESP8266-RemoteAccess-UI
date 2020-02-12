07338181dd380b96659b3e974d1818fa5abc2db4
William Yang <williamyang2017@gmail.com>
2020-02-12 19:48:32 +1100

    Base class for led controllers

--- src/LEDController/LedController.jsx
+++ src/LEDController/LedController.jsx
@@ -1,35 +1,41 @@
 import WebsocketManager from "../WebsocketManager.jsx";
 
-import Slider from "./Slider.jsx";
-import Fader from "./Fader.jsx";
-
 import React from 'react';
 import ReactDom from 'react-dom';
-
-const Controllers = {
-    Slider, Fader
-};
-
-const GetController = (props) => {
-    
-};
 
 export default class LedController extends React.Component {
     constructor(props) {
         super(props);
-        this.props.websocket.listen(this.dataListener.bind(this), WebsocketManager.EventType.DATA);
-        this.props.websocket.listen(this.getInfo.bind(this), WebsocketManager.EventType.SOCKET_STATUS);
-        this.controllers = ["Slider", "Fader"];
+        this.callbacks = [
+            [this.dataListener.bind(this), WebsocketManager.EventType.DATA],
+            [this.statusListener.bind(this), WebsocketManager.EventType.SOCKET_STATUS]
+        ];
+
+        this.callbacks.map((value) => {
+            this.props.websocket.listen(value[0], value[1]);
+        });
+
         this.state = {
-            leds: [],
-            controller: "Slider"
+            leds: []
         };
+
+        if (this.props.websocket.isActive()) {
+            this.getInfo();
+        }
     }   
+
+    componentWillUnmount() {
+        this.callbacks.map((value) => {
+            this.props.websocket.unlisten(value[0], value[1]);
+        });
+    }
 
     dataListener(data) {
         if (data.length < 2 || data[0] !== 0x01) {
             return;
         }
+
+        console.log("Getting data");
 
         let command_type = data[1];
         if (command_type === 0x02 && data.length >= 3) {
@@ -41,7 +47,7 @@ export default class LedController extends React.Component {
         return true;
     }
 
-    setLeds(leds) {
+    set leds(leds) {
         let changes = [];
         for (let i = 0; i < leds.length; i++) {
             let newValue = leds[i] & 0xFF;
@@ -59,6 +65,17 @@ export default class LedController extends React.Component {
         this.setState({...this.state, leds: leds});
     }
 
+    get leds() {
+        return this.state.leds;
+    }
+
+    statusListener(status) {
+        if (status) {
+            this.getInfo();
+        }
+    }
+
+
     onInfo(data) {
         let total_leds = data[2];
         if (total_leds !== data.length-3) {
@@ -73,46 +90,7 @@ export default class LedController extends React.Component {
         this.setState({...this.state, leds: leds});
     }
 
-    getInfo(status) {
-        if (status) {
-            this.props.websocket.send([0x01, 0x02]);
-        }
-    }
-
-    setController(controller) {
-        this.setState({...this.state, controller: controller});
-    }
-
-    getControllerComponent(props) {
-        let Controller = Controllers[this.state.controller];
-        return <Controller {...props}></Controller>;
-    }
-
-    renderControllers() {
-        return this.controllers.map((value, index) => {
-            return <a key={index} className="dropdown-item" onClick={() => this.setController(value)}>{value}</a>;
-        })
-    }
-
-    renderHeader() {
-        return (
-        <div className="d-flex justify-content-between flex-wrap flex-mid-nowrap align-items-center pb-2 mb-3 border-bottom">
-            <h3>LED Controls</h3>
-            <div className="btn-toolbar mb-2 mb-md-0 dropdown">
-                <button className="btn btn-sm btn-outline-secondary dropdown-toggle" data-toggle="dropdown">{this.state.controller}</button>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    {this.renderControllers()} 
-                </div>
-            </div>
-        </div>);
-    }
-
-
-    render() {
-        return (
-        <div>
-            {this.renderHeader()}
-            {this.getControllerComponent({leds: this.state.leds, setLeds: this.setLeds.bind(this)})}
-        </div>);
+    getInfo() {
+        this.props.websocket.send([0x01, 0x02]);
     }
 }
