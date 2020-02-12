70a4b5c0b6eaedb06b798088057910b9260b1d8a
FiendChain <formulating.response@gmail.com>
2020-02-13 00:28:10 +1100

    Updates

---
+++ precache-manifest.3b0747dd1aaa27c078987ad92faf078c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ce762a1d51b58e62a710a136343987",
+    "url": "/ESP8266-RemoteAccess-UI/index.html"
+  },
+  {
+    "revision": "2ed854f99a061abbe8ab",
+    "url": "/ESP8266-RemoteAccess-UI/static/js/2.21b0a377.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ESP8266-RemoteAccess-UI/static/js/2.21b0a377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e138e5b58fc95f95ad",
+    "url": "/ESP8266-RemoteAccess-UI/static/js/main.30831edf.chunk.js"
+  },
+  {
+    "revision": "ec26009c34b68b087ec3",
+    "url": "/ESP8266-RemoteAccess-UI/static/js/runtime-main.66c72769.js"
+  }
+]);
